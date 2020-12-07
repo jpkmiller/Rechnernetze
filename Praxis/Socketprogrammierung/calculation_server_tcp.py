c499aa4f012db8cd8ab3fb167516c221c3e786be
Alex Schapelt <alexanderschapelt>
2020-12-07 13:32:34 +0100

    added multithreading support for 4.3

--- Praxis/Socketprogrammierung/calculation_server_tcp.py
+++ Praxis/Socketprogrammierung/calculation_server_tcp.py
@@ -1,6 +1,8 @@
 import socket
 import time
 import struct
+from concurrent.futures import ThreadPoolExecutor
+
 import numpy
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,6 +23,7 @@ t_end = time.time() + server_activity_period  # Ende der Aktivitätsperiode
 
 command_length = 64
 
+exec = ThreadPoolExecutor(5)
 
 functions = {
     'Summe': lambda l: numpy.copy(l).sum(initial=0),
@@ -32,36 +35,36 @@ functions = {
 sock.listen(1)
 print('Listening ...')
 
-while time.time()<t_end:
+
+def process_data(conn, addr):
+    while time.time() < t_end:
+        try:
+            data = conn.recv(1024)
+            if not data:  # receiving empty messages means that the socket other side closed the socket
+                print('Connection closed from other side')
+                print('Closing ...')
+                conn.close()
+                break
+            (id, operator, number) = struct.unpack('>I' + str(command_length) + 'sB', data[:69])
+            operator = operator.decode('utf-8').strip('\x00')
+            operands = struct.unpack_from('>' + str(number) + 'i', data[69:])
+            result = functions[operator](operands)
+            payload = struct.pack('>Ii', 1234, result)
+            print(operator)
+            print(operands)
+            print(payload)
+            conn.send(payload)
+            conn.close()
+        except socket.timeout:
+            print('Socket timed out at', time.asctime())
+
+
+while time.time() < t_end:
     print("waiting for connection")
     try:
         conn, addr = sock.accept()
         print('Incoming connection accepted: ', addr)
-        break
+        exec.submit(process_data, conn, addr)
     except socket.timeout:
         print('Socket timed out listening', time.asctime())
         continue
-
-while time.time() < t_end:
-    try:
-        data = conn.recv(1024)
-        if not data:  # receiving empty messages means that the socket other side closed the socket
-            print('Connection closed from other side')
-            print('Closing ...')
-            conn.close()
-            break
-        (id, operator, number) = struct.unpack('>I' + str(command_length) + 'sB', data[:69])
-        operator = operator.decode('utf-8').strip('\x00')
-        operands = struct.unpack_from('>' + str(number) + 'i', data[69:])
-        result = functions[operator](operands)
-        payload = struct.pack('>Ii', 1234, result)
-        print(operator)
-        print(operands)
-        print(payload)
-        conn.send(payload)
-    except socket.timeout:
-        print('Socket timed out at', time.asctime())
-
-sock.close()
-if conn:
-    conn.close()
