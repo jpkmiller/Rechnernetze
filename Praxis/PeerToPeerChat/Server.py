91b0099dc5294855ba71742fdb12b421a7973f91
Alex Schapelt <alexanderschapelt>
2020-12-18 11:35:00 +0100

    first Server implementation. To be validated

--- Praxis/PeerToPeerChat/Server.py
+++ Praxis/PeerToPeerChat/Server.py
@@ -1,3 +1,4 @@
+import json
 import socket
 import struct
 from concurrent.futures import ThreadPoolExecutor
@@ -17,81 +18,113 @@ tcp_sock.bind((My_IP, My_PORT))
 clientList = {}
 
 
-def register_client(payload: bytes, addr, conn) -> bool:
-    nickname, udp_port = struct.unpack('>64sH', payload)
-    nickname = nickname.decode('utf-8').strip('\x00')
+def register_client(nickname, ip, port: int, conn) -> bool:
     if nickname in clientList:
         return False
 
-    number_of_clients = len(clientList)
-    response = bytearray(struct.pack('>H', number_of_clients))
-    for client, (ip_addr, port) in clientList.items():
-        response.append(*struct.pack('>64s4cH', client.encode('utf-8'), ip_addr, port))
+    added_list = [{'nickname': key, 'ip': value[0], 'port': value[1]} for key, value in clientList.items()]
+    user_list_dict = {
+        'type': 'user-list',
+        'users': {
+            'added': added_list,
+            'removed': []
+        }
+    }
 
-    conn.send(response)
+    client_list_response = json.dumps(user_list_dict)
+    client_list_msg = bytearray(client_list_response, 'utf-8')
+    msg = bytearray(len(client_list_msg))
+    msg.extend(client_list_msg)
+    conn.send(bytes(msg))
 
-    broadcast_payload = struct.pack('>32s64s4cH', 'join'.encode('utf-8'), nickname.encode('utf-8'), addr, udp_port)
+    update_user_list_dict = {
+        'type': 'user-list',
+        'users': {
+            'added': [{'nickname': nickname, 'ip': ip, 'port': port}],
+            'removed': []
+        }
+    }
+    update_list_response = json.dumps(update_user_list_dict)
+    update_list_msg = bytearray(update_list_response, 'utf-8')
+    update_msg = bytearray(len(update_list_msg))
+    update_msg.extend(update_list_msg)
+    broadcast_clients(bytes(update_msg))
 
-    clientList[nickname] = (addr, udp_port)
+    clientList[nickname] = (ip, port, conn)
     return True
 
 
-def deregister_client(payload: bytes, addr) -> bool:
-    nickname, udp_port = struct.unpack('>64sH', payload)
-    nickname = nickname.decode('utf-8').strip('\x00')
-
-    old_ip, old_port = clientList[nickname]
-    if old_ip != addr or old_port != udp_port:
+def unregister_client(nickname, addr) -> bool:
+    if nickname not in clientList or clientList[nickname][0] != addr:
         return False
-    clientList.pop(nickname)
+    removed_client = clientList.pop(nickname)
+    removed_client[3].close()
+    update_user_list_dict = {
+        'type': 'user-list',
+        'users': {
+            'added': [],
+            'removed': [{'nickname': nickname, 'ip': removed_client[0], 'port': removed_client[1]}]
+        }
+    }
+    update_list_response = json.dumps(update_user_list_dict)
+    update_list_msg = bytearray(update_list_response, 'utf-8')
+    update_msg = bytearray(len(update_list_msg))
+    update_msg.extend(update_list_msg)
+    broadcast_clients(bytes(update_msg))
     return True
 
 
-def decode_ip(addr) -> str:
-    return '.'.join(addr)
-
-
-def send_message(payload: bytes, ip_addr, port):
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.connect((decode_ip(ip_addr), port))
-    tcp_socket.settimeout(10)
-    tcp_socket.send(payload)
-
-
-broadcast_msg = None
-isBroadcasting = False
-
-
-def broadcast_clients(payload: bytes, exclude=None):
-    for nickname, (ip_addr, port) in clientList.items():
+def broadcast_clients(msg: bytes, exclude=None):
+    for nickname, (ip_addr, port, conn) in clientList.items():
         # exclude certain client from broadcast e.g. register
         if exclude is not None and nickname is exclude:
             continue
-
-        connection_request = struct.pack('>32s4cH', 'connect to', server_ip, port)
-        udp_sock.sendto()
-        conn.send(payload)
-
-
-def send_broadcast(_, addr, conn):
-    conn.send(broadcast_msg)
+        conn.send(msg)
 
 
 # deregister der form <Nickname><Port>
 message_types = {
     'register': register_client,
-    'deregister': deregister_client,
-    'broadcast': broadcast_clients,
-    'listening': send_broadcast
+    'deregister': unregister_client,
+    'broadcast': broadcast_clients
 }
 
 
 def process_request(conn, addr):
     data = conn.recv(1024)
+    size = int.from_bytes(bytes=data[:4], byteorder='little', signed=False)
+    payload = data[4:].decode('utf-8')
+    while len(payload) <= size:
+        data = conn.recv(1024)
+        payload += data.decode('utf-8')
 
-    message_type = data[:64].decode('utf-8')
-    message_types[message_type](data[64:], addr, conn)
-    pass
+    # TODO: check if this is needed
+    json.dumps(payload)
+    # -----------------------------
+
+    payload_dict = json.loads(payload)
+    msg_type = payload_dict['type']
+    try:
+        if msg_type == 'register':
+            nickname = payload['nickname']
+            ip = payload['ip']
+            port = payload['port']
+            register_client(nickname, ip, port, conn)
+
+        elif msg_type == 'unregister':
+            nickname = payload['nickname']
+            unregister_client(nickname, addr)
+
+        elif msg_type == 'broadcast':
+            nickname = payload['nickname']
+            broadcast_msg = payload['message']
+            msg = bytearray(len(broadcast_msg))
+            msg.extend(broadcast_msg)
+            broadcast_clients(msg, nickname)
+
+    except KeyError:
+        # TODO: handle malformed requests
+        pass
 
 
 if __name__ == '__main__':
