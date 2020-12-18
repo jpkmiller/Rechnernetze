5587ce6b7f1fc0da31a50dd69cc45c4e6a52afa4
jpkmiller <j.mueller5021@gmail.com>
2020-12-18 09:58:14 +0100

    PeerToPeerChat: continued Client

--- Praxis/PeerToPeerChat/Client.py
+++ Praxis/PeerToPeerChat/Client.py
@@ -1,11 +1,85 @@
 import socket
 import struct
+import json
+from sys import argv
 
 message_types = ['register', 'deregister', 'send']
 
-def register(ipAddress: str, port: int):
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.settimeout(10)
-    tcp_socket.connect((ipAddress, port))
-    register = struct()
 
+# sus schhhhhhhhhhhhapelt
+
+# json.loads(...) # decode
+# json.dumps(...) # encode
+
+
+# address = clients[nickname]
+# udp_port.sendTo(address, "connect to me at 1234"
+# tcp_port.accept()
+
+class Client:
+
+    def __init__(self, nickname: str = 'shitass', SERVER_IP: str = '127.0.0.1'):
+        self.nickname = nickname
+        self.socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ip, self.port = self.socketUDP.getsockname()  # getsockname() returns a 2-tuple (host, port)
+        self.SERVER = SERVER_IP, 2000
+        self.clients = {}
+
+        self.register()
+
+    @staticmethod
+    def convertToBytes(message: str) -> bytes:
+        sizeOfMessage = len(message)
+        return struct.pack('>I' + str(sizeOfMessage) + 's', sizeOfMessage, message)
+
+    @staticmethod
+    def sendOverTCP(address: tuple, jsonMessage: str, keepAlive: bool = False):
+        socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socketTCP.settimeout(10)
+        socketTCP.connect((address[0], address[1]))
+        bytesMessage = Client.convertToBytes(jsonMessage)  # or bytes(byteMessageSize) or byteMessageSize.to_bytes() or
+        socketTCP.send(bytesMessage)
+
+        if not keepAlive:
+            socketTCP.close()
+
+    def register(self):
+        registerDict = {'type': 'register', 'nickname': self.nickname, 'ip': self.ip, 'port': self.port}
+        self.sendOverTCP(self.SERVER, json.dumps(registerDict))
+
+    def unregister(self):
+        unregisterDict = {'type': 'unregister', 'nickname': self.nickname}
+        self.sendOverTCP(self.SERVER, json.dumps(unregisterDict))
+
+    def sendMessage(self, message: str, nickname: str):
+        c = self.clients[nickname]  # ip, port
+
+        # TODO: check if client already has a connection to other client
+        connectToClientMessage = 'Please talk to me baby on {} one more time.'.format(self.socketTCP.getsockname()[1])
+        bytesMessageRequest = Client.convertToBytes(connectToClientMessage)
+        self.socketUDP.sendto(bytesMessageRequest, c['address'])
+        self.socketTCP.accept()
+
+        # alex und walle sind geil aaaaaaaaaaaaaaaaaaaaaaa lel du geilesau josef
+        messageRequest = {'type': 'message', 'message': message}
+        bytesMessage = Client.convertToBytes(json.dumps(messageRequest))
+        self.socketTCP.send(bytesMessage)
+
+    @staticmethod
+    def receiveMessage(sock: socket):
+        try:
+            receivedMessage = sock.recv(8)
+            messageSize, _ = struct.unpack('>I', receivedMessage)
+            _, message = struct.unpack('>I' + str(messageSize) + 's', receivedMessage)
+            print(str(message))
+            decodedMessage = json.loads(message)
+            messageType = decodedMessage['type']
+            print(str(messageType))
+        except socket.timeout:
+            print('Socket timed out')
+            sock.close()
+
+
+if __name__ == '__main__':
+    client = Client(argv[1])
--- Praxis/PeerToPeerChat/Server.py
+++ Praxis/PeerToPeerChat/Server.py
@@ -3,102 +3,90 @@ import struct
 from concurrent.futures import ThreadPoolExecutor
 
 executor = ThreadPoolExecutor(9)
-My_IP = '127.0.0.1'
-server_ip = [127, 0, 0, 1]
-print(My_IP)
-My_PORT = 50000
 server_activity_period = 30
 udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_sock.bind((My_IP, My_PORT))
-
 # Nickname -> (IPAddr, Port)
-clientList = {}
+clients = {}
 
 
-def register_client(payload: bytes, addr, conn) -> bool:
-    nickname, udp_port = struct.unpack('>64sH', payload)
-    nickname = nickname.decode('utf-8').strip('\x00')
-    if nickname in clientList:
-        return False
+class Server:
 
-    number_of_clients = len(clientList)
-    response = bytearray(struct.pack('>H', number_of_clients))
-    for client, (ip_addr, port) in clientList.items():
-        response.append(*struct.pack('>64s4cH', client.encode('utf-8'), ip_addr, port))
+    def __init__(self, ip: tuple = ('127.0.0.1', 2000)):
+        self.socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socketTCP.bind(ip)
+        self.clients = {}
 
-    conn.send(response)
+    def registerClient(self, payload: bytes, addr, conn) -> bool:
+        nickname, udp_port = struct.unpack('>64sH', payload)
+        nickname = nickname.decode('utf-8').strip('\x00')
+        if nickname in clients:
+            return False
 
-    broadcast_payload = struct.pack('>32s64s4cH', 'join'.encode('utf-8'), nickname.encode('utf-8'), addr, udp_port)
+        number_of_clients = len(clients)
+        response = bytearray(struct.pack('>H', number_of_clients))
+        for client, (ip_addr, port) in clients.items():
+            response.append(*struct.pack('>64s4cH', client.encode('utf-8'), ip_addr, port))
 
-    clientList[nickname] = (addr, udp_port)
-    return True
+        conn.send(response)
 
+        broadcast_payload = struct.pack('>32s64s4cH', 'join'.encode('utf-8'), nickname.encode('utf-8'), addr, udp_port)
 
-def deregister_client(payload: bytes, addr) -> bool:
-    nickname, udp_port = struct.unpack('>64sH', payload)
-    nickname = nickname.decode('utf-8').strip('\x00')
+        clients[nickname] = (addr, udp_port)
+        return True
 
-    old_ip, old_port = clientList[nickname]
-    if old_ip != addr or old_port != udp_port:
-        return False
-    clientList.pop(nickname)
-    return True
+    def deregisterClient(self, payload: bytes, addr) -> bool:
+        nickname, udp_port = struct.unpack('>64sH', payload)
+        nickname = nickname.decode('utf-8').strip('\x00')
 
+        old_ip, old_port = self.clients[nickname]
+        if old_ip != addr or old_port != udp_port:
+            return False
+        clients.pop(nickname)
+        return True
 
-def decode_ip(addr) -> str:
-    return '.'.join(addr)
+    @staticmethod
+    def decode_ip(addr) -> str:
+        return '.'.join(addr)
 
+    def sendMessage(self, payload: bytes, ip_addr, port):
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.connect((Server.decode_ip(ip_addr), port))
+        tcp_socket.settimeout(10)
+        tcp_socket.send(payload)
 
-def send_message(payload: bytes, ip_addr, port):
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.connect((decode_ip(ip_addr), port))
-    tcp_socket.settimeout(10)
-    tcp_socket.send(payload)
+    broadcast_msg = None
+    isBroadcasting = False
 
+    def broadcast_clients(self, payload: bytes, exclude=None):
+        for nickname, (ip_addr, port) in clients.items():
+            # exclude certain client from broadcast e.g. register
+            if exclude is not None and nickname is exclude:
+                continue
 
-broadcast_msg = None
-isBroadcasting = False
+            connection_request = struct.pack('>32s4cH', 'connect to', self.server_ip, port)
+            udp_sock.sendto()
+            conn.send(payload)
 
+    def send_broadcast(self, _, addr, conn):
+        conn.send(broadcast_msg)
 
-def broadcast_clients(payload: bytes, exclude=None):
-    for nickname, (ip_addr, port) in clientList.items():
-        # exclude certain client from broadcast e.g. register
-        if exclude is not None and nickname is exclude:
-            continue
+    # deregister der form <Nickname><Port>
 
-        connection_request = struct.pack('>32s4cH', 'connect to', server_ip, port)
-        udp_sock.sendto()
-        conn.send(payload)
+    def process_request(self, conn, addr):
+        data = conn.recv(1024)
 
-
-def send_broadcast(_, addr, conn):
-    conn.send(broadcast_msg)
-
-
-# deregister der form <Nickname><Port>
-message_types = {
-    'register': register_client,
-    'deregister': deregister_client,
-    'broadcast': broadcast_clients,
-    'listening': send_broadcast
-}
-
-
-def process_request(conn, addr):
-    data = conn.recv(1024)
-
-    message_type = data[:64].decode('utf-8')
-    message_types[message_type](data[64:], addr, conn)
-    pass
+        message_type = data[:64].decode('utf-8')
+        message_types[message_type](data[64:], addr, conn)
+        pass
 
 
 if __name__ == '__main__':
+    server = Server()
     while True:
         print('waiting for connection')
         try:
-            conn, addr = tcp_sock.accept()
+            conn, addr = server.accept()
             print('Incoming connection accepted: ', addr)
             executor.submit(process_request, conn, addr)
         except socket.timeout:
