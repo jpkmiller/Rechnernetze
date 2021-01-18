c9b3d94e3fe1d4732146edc0463e94b15b5acc03
jpkmiller <j.mueller5021@gmail.com>
2021-01-18 11:26:30 +0100

    MoodleApp: receiving messages works. Pdf download and creation works.

--- Praxis/HTTP/MoodleApp.py
+++ Praxis/HTTP/MoodleApp.py
@@ -1,12 +1,9 @@
-import os
-import re
+import json
 import time
-import numpy as np
+import os
 
 import requests
 from bs4 import BeautifulSoup
-import json
-import webbrowser
 
 
 class MoodleApp:
@@ -52,6 +49,7 @@ class MoodleApp:
         self.user_id = soup.find('div', {'id': 'nav-notification-popover-container'})['data-userid']
 
     def login(self):
+        print("Logging in...")
         self.get_logintoken()
         payload = {
             'anchor': '',
@@ -66,57 +64,47 @@ class MoodleApp:
         self.get_user_id(r.text)
 
     def logout(self):
+        print("Logging out...")
         url = 'https://moodle.htwg-konstanz.de/moodle/login/logout.php?sesskey=' + self.session_key
-        r = self.session.get(url, cookies=self.cookies)
+        r = self.session.get(url, cookies=self.session.cookies)
+        if r.status_code == 200:
+            print("Logged out successfully.")
 
     def download_task(self):
-        print("downloading task..")
-        # TODO: implement method for the download of this lab exercise
-
-        # webbrowser.open("https://moodle.htwg-konstanz.de/moodle/pluginfile.php/188750/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.docx?forcedownload=1")
-        # webbrowser.open("https://moodle.htwg-konstanz.de/moodle/pluginfile.php/188750/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.pdf?forcedownload=1")
-
-        # r = self.session.get('https://moodle.htwg-konstanz.de/moodle/pluginfile.php')
-        # self.cookies = r.cookies
-
+        print("Downloading task...")
         r = self.session.get(
             'https://moodle.htwg-konstanz.de/moodle/pluginfile.php/188750/mod_assign/introattachment/0/AIN%20RN%20-%20Laboraufgabe%20-%20HTTP.pdf',
-            cookies=self.cookies, stream=True)
-        with open('Praxis/HTTP/DownloadContent/task.pdf', 'wb') as pdf:
+            cookies=self.session.cookies, stream=True)
+        if r.status_code == 200:
+            print("Received file data successfully.")
+        os.makedirs("DownloadedContent", exist_ok=True)
+        with open('DownloadedContent/HTTP - Labor.pdf', 'w+b') as pdf:
+            print("Creating file...")
             for chunk in r.iter_content(chunk_size=1024):
                 pdf.write(chunk)
+            print("Created pdf successfully.")
 
-    def fetch_notifications(self):
-        def get_context_id(session):
-            r = session.get('https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php?id=183',
-                            cookies=session.cookies)
-            return self.get_key_info(r.text, 'context_id')
-
-        JSON = [
-            {
-                "index": 0,
-                "methodname": "core_fetch_notifications",
-                "args": {
-                    "contextid": get_context_id(self.session)
-                }
-            }
-        ]
-        payload = json.dumps(JSON)
-        messages = self.session.post(
-            'https://moodle.htwg-konstanz.de/moodle/lib/ajax/service.php?sesskey=' + self.session_key + '&info=core_fetch_notifications',
-            data=payload,
-            cookies=self.session.cookies
-        )
+    @staticmethod
+    def getCookies(cookie_jar):
+        cookie_dict = cookie_jar.get_dict()
+        found = ['%s=%s' % (name, value) for (name, value) in cookie_dict.items()]
+        return ';'.join(found)
 
     def receive_message(self):
         def get_messages_from_response(response):
-            # TODO: BeautifulSoup does not work as expected here
             soup = BeautifulSoup(response, 'html.parser')
-            author = soup.find('tr').find('th', {'class': 'title'})
-            content = soup.find('tr').find('td', {'class': 'text'})
-            print(content)
-            print(author)
-            return np.asarray(author, content).T
+            if len(soup.body.find_all(text='Keine Mitteilungen gefunden')) != 0:
+                print("No messages found!")
+                return
+            chat = soup.find('table', {'class': 'generaltable'})
+            author = chat.find_all('th', {'class': 'title'})
+            content = chat.find_all('td', {'class': 'text'})
+            t = chat.find_all('td', {'class': 'c3'})
+            print("=============================================")
+            print("Received Messages:")
+            for i in range(len(content)):
+                print(f"{author[i].text}: {content[i].text} ({t[i].text})")
+            print("=============================================")
 
         last = round(time.time())
         JSON = {
@@ -128,17 +116,29 @@ class MoodleApp:
             "refresh": "Aktualisieren"
         }
 
-        r = self.session.post('https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php?id=183',
-                              data=JSON,
-                              cookies=self.session.cookies)
-        """
-        r = self.session.get(
-             'https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php?id=183',
-           cookies=self.session.cookies
-        )
-        """
-        print(r.text)
-        print(get_messages_from_response(r.text))
+        # idk why.. but Moodle needs specific headers to refresh the content
+        headers = {
+            "Host": "moodle.htwg-konstanz.de",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox\/84.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-CA,en-US;q=0.7,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": "82",
+            "Origin": "https://moodle.htwg-konstanz.de",
+            "DNT": "1",
+            "Connection": "keep-alive",
+            "Cookie": MoodleApp.getCookies(self.session.cookies),
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-GPC": "1",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache"
+        }
+
+        r = self.session.post('https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php', data=JSON,
+                              headers=headers)
+        get_messages_from_response(r.text)
 
     def chat(self, message: str):
         last = round(time.time())
@@ -154,6 +154,7 @@ class MoodleApp:
         self.session.post('https://moodle.htwg-konstanz.de/moodle/mod/chat/gui_basic/index.php?id=183',
                           data=JSON,
                           cookies=self.session.cookies)
+        time.sleep(4)
         self.receive_message()
 
     def upload_task(self):
@@ -162,14 +163,16 @@ class MoodleApp:
 
     def tui(self):
         while True:
-            i = input("Chat / Info / Download / Upload / Exit\n")
+            i = input("Chat / Refresh / Info / Download / Upload / Exit\n")
             if i == "Chat" or i == "chat":
                 while True:
-                    i = input("Message: ")
+                    i = input("Your Message: ")
                     if i == "Exit" or i == "exit":
                         break
                     self.chat(i)
                 continue
+            elif i == "Refresh" or i == "refresh":
+                self.receive_message()
             elif i == "Info" or i == "info":
                 self.get_infos()
             elif i == "Download" or i == "download":
@@ -177,7 +180,10 @@ class MoodleApp:
             elif i == "Upload" or i == "upload":
                 self.upload_task()
             elif i == "Exit" or i == "exit":
+                self.logout()
                 exit(0)
+            elif i == "switch":
+                break
 
     def get_infos(self):
         print("Test MoodleApp")
